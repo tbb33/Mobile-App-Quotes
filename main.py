d742d73e6124283b0940f94c1f1e09d68f9c6882
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-12-16 12:21:10 -0600

    Store user input in json file & signup screen
    Main.py: open users.json file, created new key/value pair from user input (from textinput in login screen), write new dict to users.json file to include new key/value pair.
    
    design.kv: created signupscreensuccess screen. Added button to take back to login screen.
    
    users.json: This is dictionary of dictionaries that store key/value pairs where key is the username and values are username, password, and datetime created. Note that to see this file in json format press shift+alt+f

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen): #screen obj
    def sign_up(self):
-       #order to display screen
        self.manager.current = "sign_up_screen"
 
 class RootWidget(ScreenManager): #inherits from Screenmanager obj
     pass
 
 class SignUpScreen(Screen):
-    def sign_up(self, uname, pword):
-        print(uname, pword)
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+            #create another pair of key/value
+            users[uname] = {'username':uname, 'password': pword,
+            'created': datetime.now().strftime("%Y-%m-%d %H-%m-%S")}
+        print(users)
+
+        #overwrite existing users.json file, write new dict
+        with open("users.json", 'w' ) as file:
+            json.dump(users, file)
+        #name screen want to switch to
+        self.manager.current = "sign_up_screen_success"
+
+class SignUpScreenSuccess(Screen):
+    def return_log_in(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "Login_screen"
 
 class MainApp(App):
     def build(self):
